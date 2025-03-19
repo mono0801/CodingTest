a993a63a89ed10f891d0d54ea1a83724d081566e
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-19 14:14:36 +0900

    [level 0] Title: 문자열 묶기, Time: 8.33 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,9 @@
+function solution(strArr) {
+    const countMap = new Map();
+
+    strArr.map((v) =>
+        countMap.set(v.length, (countMap.get(v.length) || 0) + 1)
+    );
+
+    return Math.max(...[...countMap.values()]);
+}
