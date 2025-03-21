9d6df52dded0f5aa44416f932940bf942cb10fee
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-21 09:52:56 +0900

    [level 2] Title: 점 찍기, Time: 7.07 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,23 +1,8 @@
 function solution(k, d) {
     let count = 0;
-    let limit = Math.floor(d / k);
 
-    for (let a = 0; a <= limit; a++) {
-        let x = a * k;
-        let left = 0, right = limit, bestB = 0;
-        
-        while (left <= right) {
-            let mid = Math.floor((left + right) / 2);
-            let y = mid * k;
-            if (x * x + y * y <= d * d) {
-                bestB = mid; 
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        count += bestB + 1;
+    for (let a = 0; a <= d; a += k) {
+        count += ((Math.sqrt(d * d - a * a) / k) | 0) + 1;
     }
 
     return count;
