8bb6dfbfa3b87a7a19b68aaae0fd7561361add03
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-22 20:35:24 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,8 +1,9 @@
 function solution(my_str, n) {
-    let anwser = [];
-    for (let i = 0; i < my_str.length; i += n) {
-        anwser.push(my_str.substring(i, i + n));
-    }
+    // let anwser = [];
+    // for (let i = 0; i < my_str.length; i += n) {
+    //     anwser.push(my_str.substring(i, i + n));
+    // }
 
-    return anwser;
+    // return anwser;
+    return my_str.match(new RegExp(`.{1,${n}}`, "g"));
 }
