a22461c0d8a94dc36cc398ec68c46b57c96bb012
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-22 20:01:02 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,10 +1,5 @@
-const sum = (a, n) => (n * (2 * a + (n - 1))) / 2;
-
 function solution(num, total) {
-    let a = -1000;
-    for (a; a <= 1000; a++) {
-        if (sum(a, num) === total) break;
-    }
+    const a = (2 * total / num + 1 - num) / 2;
 
     return new Array(num).fill().map((_, i) => {
         return a + i;
