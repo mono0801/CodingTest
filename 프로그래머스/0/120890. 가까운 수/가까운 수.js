0f0ac885139be1991cf062f4b82f6fed04445d4f
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-23 21:26:18 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,9 +1,10 @@
 function solution(array, n) {
-    const arr = array.map((v) => v - n);
-    const arrAbs = arr.map((v) => Math.abs(v));
-    const min = Math.min(...arrAbs);
-    const idx = arr.filter((v) => Math.abs(v) === min);
-    const i = arr.indexOf(Math.min(...idx));
+//     const arr = array.map((v) => v - n);
+//     const arrAbs = arr.map((v) => Math.abs(v));
+//     const min = Math.min(...arrAbs);
+//     const idx = arr.filter((v) => Math.abs(v) === min);
+//     const i = arr.indexOf(Math.min(...idx));
 
-    return array[i];
+//     return array[i];
+      return array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b)[0];
 }
