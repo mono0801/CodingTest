0771b2627f569aa69a896d16566737e8bde22c24
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-23 20:43:31 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    let arr = [...my_string];
+    const temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
+
+    return arr.join("");
+}
