7c98d35541c665f34a27a7a1ea9914f9b8ffd9ef
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-23 21:35:46 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,6 +1,7 @@
 function solution(my_string) {
-    const countMap = {};
+    //const countMap = {};
 
-    [...my_string].map((v) => (countMap[v] = 0));
-    return Object.keys(countMap).join("");
+    //[...my_string].map((v) => (countMap[v] = 0));
+    //return Object.keys(countMap).join("");
+    return [...new Set(my_string)].join("");
 }
