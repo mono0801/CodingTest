1381c6f0af7a85c62285e03edb4c7a2c5f1d2736
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-24 20:47:15 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,12 @@
+const toDecimal = (num) => {
+    let dec = 0;
+    const arr = [...num].reverse();
+    for (let i = 0; i < arr.length; i++) {
+        dec += Number(arr[i]) * 2 ** i;
+    }
+    return dec;
+};
+
 function solution(bin1, bin2) {
     return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
 }
