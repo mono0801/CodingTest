58695c2859faaed6b66f094a3731a86a42f3ed99
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-25 23:21:57 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,4 @@
+function solution(numbers) {
+    numbers.sort((a, b) => a - b);
+    return numbers[0] * numbers[1] >= numbers.at(-1) * numbers.at(-2) ? numbers[0] * numbers[1] : numbers.at(-1) * numbers.at(-2)
+}
