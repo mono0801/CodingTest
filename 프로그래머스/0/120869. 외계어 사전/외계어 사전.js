84c367abf6b25965836ef143b15505a7d2fcc745
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-26 00:31:02 +0900

    [level 0] Title: 외계어 사전, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,14 +1,16 @@
 function solution(spell, dic) {
-    let answer = 2;
-    dic.map((v) => {
-        if (v.length === spell.length) {
-            for (let i = 0; i < spell.length; i++) {
-                v = v.replace(new RegExp(spell[i]), "");
-            }
-        }
-        if (v.length === 0) answer = 1;
-        return v;
-    });
+    // let answer = 2;
+    // dic.map((v) => {
+    //     if (v.length === spell.length) {
+    //         for (let i = 0; i < spell.length; i++) {
+    //             v = v.replace(new RegExp(spell[i]), "");
+    //         }
+    //     }
+    //     if (v.length === 0) answer = 1;
+    //     return v;
+    // });
+    // return answer;
 
-    return answer;
+    const check = spell.sort().join("");
+    return dic.some((v) => [...v].sort().join("") === check) ? 1 : 2;
 }
