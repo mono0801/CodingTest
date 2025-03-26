2c304e7f815ad7a4cf060ab99b258e239644463e
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-27 00:11:40 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,16 +1,17 @@
 function solution(age) {
-    const old = {
-        0: "a",
-        1: "b",
-        2: "c",
-        3: "d",
-        4: "e",
-        5: "f",
-        6: "g",
-        7: "h",
-        8: "i",
-        9: "j",
-    };
+    // const old = {
+    //     0: "a",
+    //     1: "b",
+    //     2: "c",
+    //     3: "d",
+    //     4: "e",
+    //     5: "f",
+    //     6: "g",
+    //     7: "h",
+    //     8: "i",
+    //     9: "j",
+    // };
+    // return [...("" + age)].reduce((acc, cur) => (acc += old[+cur]), "");
 
-    return [...("" + age)].reduce((acc, cur) => (acc += old[+cur]), "");
+    return [...("" + age)].map((v) => "abcdefghij"[v]).join("");
 }
