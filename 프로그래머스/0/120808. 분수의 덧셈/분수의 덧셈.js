07b23903458c8030ead8b39288f92aeb9b9158b6
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-28 21:20:46 +0900

    [level 0] Title: 분수의 덧셈, Time: 26.84 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,16 +1,16 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let numer = BigInt(numer1 * denom2 + numer2 * denom1);
-    let denom = BigInt(denom1 * denom2);
+    let numer = numer1 * denom2 + numer2 * denom1;
+    let denom = denom1 * denom2;
 
-    for (let i = 2n; i <= numer; ) {
+    for (let i = 2; i <= numer; ) {
         if (!(numer % i) && !(denom % i)) {
             numer = numer / i;
             denom = denom / i;
-            i = 2n;
+            i = 2;
         } else {
             i++;
         }
     }
 
-    return [Number(numer), Number(denom)];
+    return [numer, denom];
 }
