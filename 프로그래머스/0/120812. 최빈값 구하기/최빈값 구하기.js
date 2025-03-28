959ba674899de564c7e5aa122285294d6811f876
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-28 20:56:06 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -3,13 +3,9 @@ function solution(array) {
 
     array.map((v) => countMap.set(v, (countMap.get(v) || 0) + 1));
 
-    const answer = [...countMap.keys()]
-        .map((v) => [v, countMap.get(v)])
-        .sort((a, b) => b[1] - a[1]);
+    const answer = [...countMap].sort((a, b) => b[1] - a[1]);
 
-    return answer.filter(
-        ([num, cnt]) => cnt === Math.max(...[...countMap.values()])
-    ).length != 1
-        ? -1
-        : answer[0][0];
+    return answer.length === 1 || answer[0][1] > answer[1][1]
+        ? answer[0][0]
+        : -1;
 }
