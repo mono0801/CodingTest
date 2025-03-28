abec181f2421328cb16eff23da897696bc56eb97
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-28 21:23:04 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,16 +1,23 @@
+const gcd = (numer, denom) =>
+    numer % denom ? gcd(denom, numer % denom) : denom;
+
 function solution(numer1, denom1, numer2, denom2) {
     let numer = numer1 * denom2 + numer2 * denom1;
     let denom = denom1 * denom2;
 
-    for (let i = 2; i <= numer; ) {
-        if (!(numer % i) && !(denom % i)) {
-            numer = numer / i;
-            denom = denom / i;
-            i = 2;
-        } else {
-            i++;
-        }
-    }
+//     for (let i = 2; i <= numer; ) {
+//         if (!(numer % i) && !(denom % i)) {
+//             numer = numer / i;
+//             denom = denom / i;
+//             i = 2;
+//         } else {
+//             i++;
+//         }
+//     }
 
-    return [numer, denom];
+//     return [numer, denom];
+    
+    const euclid = gcd(numer, denom);
+
+    return [numer / euclid, denom / euclid];
 }
