434642bcbdf89b70a6aaa8fa60e32a0004605154
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-29 21:12:03 +0900

    [level 2] Title: 전화번호 목록, Time: 1985.09 ms, Memory: 567 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor() {
+        this.data = null;
+        this.next = {};
+    }
+}
+
+class Trie {
+    constructor() {
+        this.head = new Node();
+    }
+
+    insert(str, data) {
+        let curNode = this.head;
+
+        for (const c of [...str]) {
+            if (curNode.data) {
+                data.answer = false;
+                break;
+            }
+            if (!curNode.next[c]) curNode.next[c] = new Node();
+            curNode = curNode.next[c];
+        }
+
+        curNode.data = str;
+    }
+}
+
+function solution(phone_book) {
+    const data = {
+        answer: true,
+    };
+
+    const trie = new Trie();
+    for (const phone of phone_book.sort()) {
+        trie.insert(phone, data);
+        if (!data.answer) break;
+    }
+
+    return data.answer;
+}
