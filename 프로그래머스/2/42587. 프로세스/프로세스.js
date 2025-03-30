b320e35a81aacbb67104a9741ec0e526544825a4
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-30 21:17:29 +0900

    [level 2] Title: 프로세스, Time: 17.52 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,20 @@
 function solution(priorities, location) {
     const arr = priorities.map((v, i) => [v, i]);
     const answer = [];
+    let cnt = 0;
 
     while (arr.length > 0) {
         let [prior, idx] = arr.shift();
+        console.log([prior, idx]);
 
         if (arr.find(([v, i]) => v > prior)) {
             arr.push([prior, idx]);
         } else {
             answer.push([prior, idx]);
+            cnt++;
+            if (priorities[location] === prior && location === idx) break;
         }
     }
 
-    return (
-        answer.findIndex(
-            (subArr) =>
-                subArr[0] === priorities[location] && subArr[1] === location
-        ) + 1
-    );
+    return cnt;
 }
