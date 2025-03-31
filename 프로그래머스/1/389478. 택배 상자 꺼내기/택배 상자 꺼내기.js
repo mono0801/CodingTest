cfa20c7b9cc2645641453acbce4afdaf99b8b6ba
sellen <59917628+mono0801@users.noreply.github.com>
2025-03-31 19:37:55 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,8 +1,9 @@
 function solution(n, w, num) {
+    /*
     const arr = new Array(Math.ceil(n / w))
         .fill()
         .map((_) => new Array(w).fill(0));
-
+        
     for (let i = 1; i <= n; ) {
         let j = 0;
         for (; j < w; j++) {
@@ -19,14 +20,27 @@ function solution(n, w, num) {
             return v.reverse();
         }
     });
+    */
+
+    const arr = Array.from({ length: Math.ceil(n / w) }, () =>
+        Array(w).fill(0)
+    );
+
+    let i = 1;
+    for (let row = 0; row < arr.length; row++) {
+        for (let col = 0; col < w && i <= n; col++) {
+            arr[row][col] = i++;
+        }
+        if (row % 2) arr[row].reverse();
+    }
 
     const rowIndex = arr.findIndex((row) => row.includes(num));
     const colIndex = arr[rowIndex].indexOf(num);
 
-    const answer = [];
+    let cnt = 0;
     for (let i = rowIndex; i < arr.length; i++) {
-        if (arr[i][colIndex] != 0) answer.push(arr[i][colIndex]);
+        if (arr[i][colIndex] != 0) cnt++;
     }
 
-    return answer.length;
+    return cnt;
 }
