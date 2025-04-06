120c15ef9a9a4aa68326aa71d043280749bea55f
sellen <59917628+mono0801@users.noreply.github.com>
2025-04-06 23:53:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 20.46 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -0,0 +1,39 @@
+function solution(jobs) {
+    jobs.sort(([e, t], [E, T]) => E - e);
+    const length = jobs.length;
+    const queue = [];
+    const answer = [];
+    let time = 0;
+
+    console.log(jobs);
+
+    while (true) {
+        let last;
+        if (jobs.length) {
+            last = jobs.at(-1);
+            while (true) {
+                if (!jobs.length || last[0] > time) break;
+                queue.push(jobs.pop());
+                last = jobs.at(-1);
+            }
+        }
+
+        if (!queue.length) {
+            last = jobs.at(-1);
+            time = last[0];
+        } else {
+            queue.sort(([e, t], [E, T]) => t - T || e - E);
+            time += queue[0][1];
+            answer.push([...queue.shift(), time]);
+        }
+
+        if (!queue.length && !jobs.length) break;
+    }
+
+    const total = answer.reduce(
+        (acc, [enter, task, end]) => (acc += end - enter),
+        0
+    );
+
+    return ~~(total / length);
+}
