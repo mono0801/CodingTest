a3ba3ebbc82be2502ffc1ad2fb3d81763f9116ce
sellen <59917628+mono0801@users.noreply.github.com>
2025-04-10 21:09:34 +0900

    [level 0] Title: 2의 영역, Time: 2.11 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,10 +1,5 @@
 function solution(arr) {
-    console.log(process.version);
-
-    arr = arr.slice(arr.findIndex((v) => v === 2));
-    arr = arr.reverse()
-        .slice(arr.findIndex((v) => v === 2))
-        .reverse();
-
-    return arr.length === 1 && arr[0] != 2 ? [-1] : arr;
+    if (arr.some((v) => v === 2))
+        return arr.slice(arr.indexOf(2), arr.lastIndexOf(2) + 1);
+    return [-1];
 }
