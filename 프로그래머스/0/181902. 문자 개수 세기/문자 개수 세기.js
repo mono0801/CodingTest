6967ed9c1dd6f494d018a401737b83053cb33bde
sellen <59917628+mono0801@users.noreply.github.com>
2025-04-28 22:43:52 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     const result = new Array(52).fill(0);
 
-    [...my_string].map((v) => {
+    [...my_string].forEach((v) => {
         result[
             v.codePointAt() > 90 ? v.codePointAt() - 71 : v.codePointAt() - 65
         ]++;
